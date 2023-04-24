c04bb4112586fb1ff06e4a85b152c95dc1ea599d
Pete Favelle <pete@fsquared.co.uk>
2023-04-24 12:17:49 +0100

    Quote pulled from remote, controls, all working!

--- README.md
+++ README.md
@@ -1,53 +1,28 @@
-[![CMake](https://github.com/ahnlak-rp2040/picow-boilerplate/actions/workflows/cmake.yml/badge.svg)](https://github.com/ahnlak-rp2040/picow-boilerplate/actions/workflows/cmake.yml)
+[![CMake](https://github.com/ahnlak-rp2040/gu-scolling-quote/actions/workflows/cmake.yml/badge.svg)](https://github.com/ahnlak-rp2040/gu-scolling-quote/actions/workflows/cmake.yml)
 
-# PicoW C/C++ Boilerplate Project
+# Galactic Unicorn Scrolling Quote Demo
 
-This is a template repository, inspired by and based on Pimoroni's 
-[Pico C++ Boilerplate Project](https://github.com/pimoroni/pico-boilerplate). 
-It is specifically designed to help leverage the additional features of the
-PicoW, but obviously parts of it that do not require the WiFi / Bluetooth
-functionality will be transferable to a regular Pico.
+This is small demonstration of pulling a quote from a remote web server, and
+scrolling it on the Unicorn.
 
-It is intended as a useful starting point for working with PicoW-based projects
-using the Raspberry Pi Pico C/C++ SDK; it also bakes in support for the Pimoroni
-Pico Libraries, although it does not depend on these at all.
+This is provided as an example of the [PicoW Boilerplate](https://github.com/ahnlak-rp2040/picow-boilerplate)
+in action; it uses both the `config` and `httpclient` libraries from there
+to manage the fetching of data.
 
-You generally want to be working with the latest release of the Raspberry Pi
-Pico C/C++ SDK; specifically, you will need to be on at least 1.5.0 to have
-support for all the features we have here.
+Once you copy the `uf2` onto your [Galactic Unicorn](https://shop.pimoroni.com/products/galactic-unicorn)
+and the device has rebooted, it should mount a 'PicoW' drive on which you will
+find the `CONFIG.TXT` file. Adding your WiFi credentials to this file should
+prompt the device to fetch a suitable quote and start scrolling it.
 
+The quote is pulled from [Quotable](https://github.com/lukePeavey/quotable),
+largely because it (a) doesn't require authentication and (b) doesn't require
+TLS1.3!
 
-## Features
+## Controls
 
-* ready to build project framework, including Github Actions for builds and releases
-* support for a USB Mass Storage mode, to make it easy to provide a configuration
-  file to your PicoW project (for example, providing WiFi settings) without the
-  need to recompile.
-* A collection of optional additional lightweight libraries for specific tasks:
-  - `config` provides basic handling for configuration files stored on the
-  internal filesystem provided by USBFS.
-  - `httpclient` provides a simple mechanism for retrieving data from a
-  web server.
+The LUX buttons adjust the brightness of the message.
 
-## Documentation
-
-I've attempted to keep everything as comprehensively documented as possible;
-this can be found in the `/docs` folder of this repository, and on
-[PicoW C/C++ Boilerplate GitHub Pages](https://ahnlak-rp2040.github.io/picow-boilerplate).
+Pressing the 'A' button will request a fresh quote from the server.
 
 
-## Further Examples
-
-These small projects are all based on this Boilerplate, and demonstrate something
-specific in a hopefully well-explained way.
-
-* tba
-
-
-## License
-
-This template is nominally released under the BSD 3-Clause License, to match that
-used in the SDK and to be as permissive as possible.
-
-
-Share and Enjoy
+Share & Enjoy.
--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 /*
- * main.cpp - part of the PicoW C/C++ Boilerplate Project
+ * main.cpp - part of the Galactic Unicorn Scrolling Quote demo.
  *
  * This file defines the main() function, the entrypoint for your program.
  *
- * After the general setup, it simply blinks the LED on your PicoW as the
- * traditional 'Hello World' of the Pico!
+ * This program fetches a quote from https://quotes.rest/
  *
  * Copyright (C) 2023 Pete Favelle <ahnlak@ahnlak.com>
  * This file is released under the BSD 3-Clause License; see LICENSE for details.
@@ -21,15 +20,68 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+#include "pico_graphics.hpp"
+#include "galactic_unicorn.hpp"
+#include "font8_data.hpp"
+
 
 /* Local header files. */
 
 #include "usbfs.h"
 #include "opt/config.h"
 #include "opt/httpclient.h"
+#include "okcolor.hpp"
 
 
 /* Functions. */
+
+#define MESSAGE_LENGTH    130
+
+/*
+ * extract_message - pulls the content out of the returned JSON; we are only
+ *                   interested in the 'content' block.
+ */
+
+bool extract_message( const char *p_source, char *p_target, int p_targetlen )
+{
+  const char *l_startptr;
+  const char *l_endptr;
+  int         l_message_length;
+
+  /* Try and find the content element. */
+  l_startptr = strstr( p_source, "\"content\"" );
+  if ( l_startptr == NULL )
+  {
+    return false;
+  }
+
+  /* Move onto the body of that element. */
+  l_startptr += 9;
+  while ( ( *l_startptr == ' ' ) || ( *l_startptr == ':' ) || ( *l_startptr == '\"' ) )
+  {
+    l_startptr++;
+  }
+
+  /* Now look for the closing quote on that body. */
+  l_endptr = strchr( l_startptr, '\"' );
+  if ( l_endptr == NULL )
+  {
+    return false;
+  }
+
+  /* Then simply copy the body into the target, paying attention to the length. */
+  l_message_length = l_endptr - l_startptr;
+  if ( l_message_length >= p_targetlen )
+  {
+    l_message_length = p_targetlen-1;
+  }
+  strncpy( p_target, l_startptr, l_message_length );
+  p_target[l_message_length] = '\0';
+
+  /* All done then! */
+  return true;
+}
+
 
 /*
  * main() - the entrypoint of the application; this is what runs when the PicoW
@@ -38,9 +90,12 @@
 
 int main()
 {
-  int blink_rate = 250;
-  httpclient_request_t *http_request;
-
+  float                             l_scroll_offset;
+  int                               l_message_width;
+  char                              l_scroll_message[MESSAGE_LENGTH+1];
+  httpclient_request_t             *l_http_request;
+  pimoroni::PicoGraphics_PenRGB888  l_graphics( 53, 11, NULL );
+  pimoroni::GalacticUnicorn         l_unicorn;
 
   /* Initialise stdio handling. */
   stdio_init_all();
@@ -58,7 +113,6 @@ int main()
   /* Declare some default configuration details. */
   config_t default_config[] = 
   {
-    { "BLINK_RATE", "250" },
     { "WIFI_SSID", "my_network" },
     { "WIFI_PASSWORD", "my_password" },
     { "", "" }
@@ -70,17 +124,15 @@ int main()
   /* Save it straight out, to preserve any defaults we put there. */
   config_save();
 
-  /* See if we have a blink rate in there. */
-  const char *blink_rate_string = config_get( "BLINK_RATE" );
-  if ( ( blink_rate_string != NULL ) &&
-       ( atoi( blink_rate_string ) > 0 ) )
-  {
-    blink_rate = atoi( blink_rate_string );
-  }
+  /* Initialise the Galactic Unicorn and associated graphics. */
+  l_unicorn.init();
+  l_graphics.set_font( &font8 );
+  l_scroll_offset = -53.0f;
+  strcpy( l_scroll_message, "Loading..." );
 
   /* Set up a simple web request. */
   httpclient_set_credentials( config_get( "WIFI_SSID" ), config_get( "WIFI_PASSWORD" ) );
-  http_request = httpclient_open( "https://httpbin.org/get", NULL, 1024 );
+  l_http_request = httpclient_open( "https://api.quotable.io/random", NULL, 1024 );
 
   /* Enter the main program loop now. */
   while( true )
@@ -88,38 +140,79 @@ int main()
     /* Monitor the configuration file. */
     if ( config_check() )
     {
-      /* This indicates the configuration has changed - handle it if required. */
-      blink_rate_string = config_get( "BLINK_RATE" );
-      if ( ( blink_rate_string != NULL ) &&
-           ( atoi( blink_rate_string ) > 0 ) )
-      {
-        blink_rate = atoi( blink_rate_string );
-      }
-
       /* Switch to potentially new WiFi credentials. */
       httpclient_set_credentials( config_get( "WIFI_SSID" ), config_get( "WIFI_PASSWORD" ) );
     }
 
     /* Service the http request, if still active. */
-    if ( http_request != NULL )
+    if ( l_http_request != NULL )
     {
-      httpclient_status_t l_status = httpclient_check( http_request );
-      if ( l_status == HTTPCLIENT_COMPLETE )
+      httpclient_status_t l_status = httpclient_check( l_http_request );
+      if ( ( l_status == HTTPCLIENT_COMPLETE ) || ( l_status == HTTPCLIENT_TRUNCATED ) )
       {
-        printf( "HTTP response code %d\n", http_request->http_status );
-        printf( "Response:\n%s\n", httpclient_get_response( http_request ) );
-        httpclient_close( http_request );
-        http_request = NULL;
+        /* Try and find the quote  in the response. */
+        if ( extract_message( httpclient_get_response( l_http_request ), l_scroll_message, MESSAGE_LENGTH ) )
+        {
+          /* Successfully found it, so reset the scroll offset. */
+          l_scroll_offset = -53.0f;
+        }
+
+        /* And close down the request. */
+        httpclient_close( l_http_request );
+        l_http_request = NULL;
       }
     }
 
-    /* The blink is very simple, just toggle the GPIO pin high and low. */
-    printf( "Blinking at a rate of %d ms\n", blink_rate );
-    cyw43_arch_gpio_put( CYW43_WL_GPIO_LED_PIN, 1 );
-    usbfs_sleep_ms( blink_rate );
+    /* Handle user input to adjust the brightness of the Unicorn. */
+    if( l_unicorn.is_pressed( pimoroni::GalacticUnicorn::SWITCH_BRIGHTNESS_UP ) )
+    {
+      l_unicorn.adjust_brightness( +0.01 );
+    }
+    if( l_unicorn.is_pressed( pimoroni::GalacticUnicorn::SWITCH_BRIGHTNESS_DOWN ) )
+    {
+      l_unicorn.adjust_brightness( -0.01 );
+    }
 
-    cyw43_arch_gpio_put( CYW43_WL_GPIO_LED_PIN, 0 );
-    usbfs_sleep_ms( blink_rate );
+    /* And allow button A to request a fresh quote. */
+    if ( l_unicorn.is_pressed( pimoroni::GalacticUnicorn::SWITCH_A ) )
+    {
+      /* Only try if we're not already trying. */
+      if ( l_http_request == NULL )
+      {
+        l_http_request = httpclient_open( "https://api.quotable.io/random", NULL, 1024 );
+      }
+    }
+
+    /* Render the text, scrolled appropriately. */
+    l_message_width = l_graphics.measure_text( l_scroll_message, 1 );
+    l_scroll_offset += 0.25f;
+
+    /* Wrap the message. */
+    if ( l_scroll_offset > l_message_width )
+    {
+      l_scroll_offset = -53.0f;
+    }
+
+    /* Clear the display. */
+    l_graphics.set_pen( 0, 0, 0 );
+    l_graphics.clear();
+
+    /*
+     * Work out a suitable colour to use - this pretty much lifted straight 
+     * from Pimoroni's original scrolltext demo
+     */
+    ok_color::HSL l_hsl{ l_scroll_offset/100.0f, 1.0f, 0.5f };
+    ok_color::RGB l_rgb = ok_color::okhsl_to_srgb( l_hsl );
+
+    /* Set the pen to that colour, and render the text. */
+    l_graphics.set_pen( l_rgb.r*255, l_rgb.g*255, l_rgb.b*255 );
+    l_graphics.text( l_scroll_message, pimoroni::Point( 0-l_scroll_offset, 2 ), -1, 0.55 );
+
+    /* Finally, render all that onto the Unicorn. */
+    l_unicorn.update( &l_graphics );
+
+    /* And wait a brief moment. */
+    usbfs_sleep_ms( 10 );
   }
 
   /* We would never expect to reach an end....! */
